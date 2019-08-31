3d0121d7dc67d8688e936c9cd67f3672557d3562
Ian Diaz <idiaz@itba.edu.ar>
2019-08-31 15:11:27 -0300

    Agregue que me muestre las ecuaciones

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ def main():
     sy.init_printing()
     stringParser = sp.stringParser()
     circuit = cs.circuitSolver()
+    expressionList = []
+    labelList = []
+    
     window = tk.Tk()
     window.title("Circuit Solver")
     window.geometry('600x400')
     txt = tk.Entry(window,width=20)
     txt.grid(column=3, row=1)
     txt.focus()
-    btn = tk.Button(window, text="Set Equation", command=lambda : clicked(txt.get(),stringParser))
+    for i in range(0,20):
+        labelList.append(tk.Label(window))
+        labelList[i].grid(column = 3, row = 5+i)
+    btn = tk.Button(window, text="Set Equation", command=lambda : clicked(txt.get(),stringParser, expressionList,labelList ))
     btn.grid(column=3, row=2)
     btn2 = tk.Button(window, text="Calculate", command=lambda : calculate(stringParser,circuit))
     btn2.grid(column=3, row=3)
@@ -26,15 +32,23 @@ def main():
     btn3.grid(column=4, row=2)
     window.mainloop()
 
-def clicked(string,stParser):
+def clicked(string,stParser, express, labelList):
     stParser.decode(string)
+    string = string + ' = 0'
+    express.append(string)
+    for i in range(0,len(express)):
+        labelList[i].configure(text = express[i])
     return
+
 def calculate(stParser, circuitSolver):
     circuitSolver.setEquations(stParser.getExpressionList())
     circuitSolver.setUnknowns(stParser.getUnknowns())
     circuitSolver.solveCircuit()
     print(circuitSolver.getH())
     return
+
 def parseUnk(stParser, string):
     stParser.setUnknowns(string)
+    return
+
 main()
